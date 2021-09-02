f694e8700da6f0aacd931744b05221dc34720095
sayednadim <smnadimuddin@gmail.com>
2021-09-03 06:44:18 +0900

    update functions

--- vision_toolbox/utils/helper_functions.py
+++ vision_toolbox/utils/helper_functions.py
@@ -38,36 +38,3 @@ def flist_reader(flist):
             image_path = line.strip().split()
             image_path_list.append(image_path)
     return natsort.natsorted(image_path_list)
-
-
-# tensor functions
-def normalize_tensor(tensor_image):
-    return tensor_image.mul_(2).add_(-1)
-
-
-def denormalize_tensor(tensor_image):
-    return tensor_image.add_(1).div_(2)
-
-
-def reduce_mean(tensor, axis=None, keepdim=False):
-    if not axis:
-        axis = range(len(tensor.shape))
-    for i in sorted(axis, reverse=True):
-        tensor = torch.mean(tensor, dim=i, keepdim=keepdim)
-    return tensor
-
-
-def reduce_sum(tensor, axis=None, keepdim=False):
-    if not axis:
-        axis = range(len(tensor.shape))
-    for i in sorted(axis, reverse=True):
-        tensor = torch.sum(tensor, dim=i, keepdim=keepdim)
-    return tensor
-
-
-def reduce_std(tensor, axis=None, keepdim=False):
-    if not axis:
-        axis = range(len(tensor.shape))
-    for i in sorted(axis, reverse=True):
-        tensor = torch.std(tensor, dim=i, keepdim=keepdim)
-    return tensor
